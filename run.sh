80424998a326a4bbfbf3e18c307a9fb732fe613c
root <n.muller@treeptik.fr>
2018-03-19 10:50:51 +0000

    Rendre generique .env

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker-machine create master1
+PUBLIC_IP=`/sbin/ifconfig eth0 | grep "inet ad" | cut -f2 -d: | awk '{print $1}'`
+echo "IP PUBLIC : " $PUBLIC_IP
+
+# Configure environment
+cp -f .env.template .env
+PATTERN="s/#IP#/$PUBLIC_IP/"
+sed -i $PATTERN .env
+
+# Start Docker Stack
+docker network create skynet
+docker-compose up -d
